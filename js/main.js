fa99ba89b25837809a73d2e7810ea1465c1eb644
Luskans <sylvain.migne@gmail.com>
2023-06-09 13:11:37 +0200

    add v1 of website

---
+++ js/main.js
@@ -0,0 +1,51 @@
+// Récupération des données depuis le fichier JSON
+const reponse = await fetch('data/moovies.json');
+const movies = await reponse.json();    // transforme les données json en object javascript
+// Peut aussi s'écrire :
+// const movies = await fetch('data/moovies.json').then(movies => moovies.json());
+
+function generateSections(movies){     // function qui génère toute la page web
+    for (let i = 0; i < movies.length; i++) {
+        const movie = movies[i];
+        const sectionMovie = document.createElement("section");
+        const b_Image_Text = document.createElement("div");
+        b_Image_Text.classList.add("b_Image_Text");
+        const b_Image = document.createElement("div");
+        b_Image.classList.add("b_Image");
+        const b_Text = document.createElement("div");
+        b_Text.classList.add("b_Text");
+        const b_Notation = document.createElement("div");
+        b_Notation.classList.add("b_Notation");
+        const b_Not1 = document.createElement("div");
+        b_Not1.classList.add("b_Not1");
+        b_Not1.innerText = "Notation:";
+        const b_Not2 = document.createElement("div");
+        b_Not2.classList.add("b_Not2");
+        b_Not2.innerText = movie.Metascore;
+        const b_Not3 = document.createElement("div");
+        b_Not3.classList.add("b_Not3");
+        b_Not3.innerText = movie.imdbRating;
+        const imageElement = document.createElement("img");
+        imageElement.src = movie.Images[0];
+        const titleElement = document.createElement("h2");
+        titleElement.innerText = movie.Title;
+        const plotElement = document.createElement("p");
+        plotElement.innerText = movie.Plot;
+
+        // Récupération de l'élément du DOM qui accueillera les fiches
+        const sectionMain = document.querySelector("main");
+        // Relier les balises crées au parent
+        sectionMain.appendChild(sectionMovie);
+        sectionMovie.appendChild(b_Image_Text);
+        b_Image_Text.appendChild(b_Image);
+        b_Image_Text.appendChild(b_Text);
+        b_Image.appendChild(imageElement);
+        b_Text.appendChild(titleElement);
+        b_Text.appendChild(plotElement);
+        b_Text.appendChild(b_Notation);
+        b_Notation.appendChild(b_Not1);
+        b_Notation.appendChild(b_Not2);
+        b_Notation.appendChild(b_Not3);
+    }
+}
+generateSections(movies);
